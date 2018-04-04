f96b88878b6cca337ddb254a45e8018e82730884
alex.mihaes@protonmail.com <alex.mihaes@protonmail.com>
2018-04-04 16:40:03 +0300

    Board.addWord

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,10 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Board {
+    private HashMap<String, String> wordsMap;
+
+    void addWord(Player player, String word) {
+        wordsMap.put(player.getName(), word);
+    }
 }
